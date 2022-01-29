ffbe105c56cf38233e666b590b2e77179583249b
Ryeowon Hong <ryeowon13@gmail.com>
2022-01-29 21:43:54 +0900

    Klip API execute contract

--- src/App.js
+++ src/App.js
@@ -9,15 +9,20 @@ function App() {
   const [balance, setBalance] = useState("0");
   const [qrvalue, setQrvalue] = useState(DEFAULT_QR_CODE);
 
-  const onClickgetAddress = () => {
+  const onClickGetAddress = () => {
     KlipAPI.getAddress(setQrvalue);
+  };
+
+  const onClickgSetCount = () => {
+    KlipAPI.setCount(2000, setQrvalue);
   };
 
   readCount();
   getBalance("0x6867ea7ff72efa74da0a1f4c9aa52e7215a84029");
   return (
     <div>
-      <button onClick={onClickgetAddress}>주소 가져오기</button>
+      <button onClick={onClickGetAddress}>주소 가져오기</button>
+      <button onClick={onClickgSetCount}>카운트 값 변경</button>
       <br />
       <QRCode value={qrvalue} />
     </div>
