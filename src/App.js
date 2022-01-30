e9fe609804ec1c7f2abfcea55f9537db4726aef2
Ryeowon Hong <ryeowon13@gmail.com>
2022-01-30 15:55:30 +0900

    fetch NFTs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { getBalance, readCount, setCount } from "./api/UseCaver";
+import { fetchCardsOf, getBalance, readCount, setCount } from "./api/UseCaver";
 import React, { useState } from "react";
 import * as KlipAPI from "./api/UseKilp";
 import QRCode from "qrcode.react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Alert, Container } from "react-bootstrap";
+import { Alert, Card, Container } from "react-bootstrap";
 import "./App.css";
 import "./market.css";
+import { MARKET_CONTRACT_ADDRESS } from "./constants";
 
 const DEFAULT_QR_CODE = "DEFAULT";
 const DEFAULT_ADDRESS = "0x00";
 
 function App() {
-  const [nfts, setNfts] = useState([]);
+  const [nfts, setNfts] = useState([]); // {tokenId: '101', tokenUri = ' '}
   const [myBalance, setMyBalance] = useState("0");
   const [myAddress, setMyAddress] = useState(DEFAULT_ADDRESS);
 
@@ -24,6 +25,19 @@ function App() {
   // onClickMarktetCard
   // getUserData
   // getBalance
+  const fetchMarketNFTs = async () => {
+    const _nfts = await fetchCardsOf(MARKET_CONTRACT_ADDRESS);
+    setNfts(_nfts);
+  };
+
+  const fetchMyNFTs = async () => {
+    // balanceOf -> 내가 가진 전체 NFT 개수
+    // tokenOfOwnerByIndex -> 내가 가진 NFT token ID 하나씩 가져옴
+    // tokenURI -> tokenID를 이용해 toeknURI를 가져옴
+
+    const _nfts = await fetchCardsOf(myAddress);
+    setNfts(_nfts);
+  };
 
   const getUserData = () => {
     KlipAPI.getAddress(setQrvalue, async (address) => {
@@ -42,10 +56,16 @@ function App() {
         <Alert
           onClick={getUserData}
           variant={"balance"}
-          style={{ backgroundColor: "#ccebff", fontSize: 25 }}
+          style={{ backgroundColor: "#ccebff", fontSize: 25, color: "black" }}
         >
           {myBalance}KLAY
         </Alert>
+
+        <div className="container">
+          {nfts.map((nft, idx) => {
+            return <Card.Img className="img-responsive" src={nfts[idx].uri} />;
+          })}
+        </div>
       </div>
 
       <Container
@@ -58,6 +78,9 @@ function App() {
       >
         <QRCode value={qrvalue} size={256} style={{ margin: "auto" }} />
       </Container>
+      <button style={{ color: "white" }} onClick={fetchMyNFTs}>
+        aaa
+      </button>
     </div>
   );
 }
--- src/api/UseCaver.js
+++ src/api/UseCaver.js
@@ -1,11 +1,15 @@
 import Caver from "caver-js";
 import CounterABI from "../abi/CounterABI.json";
+import KIP17ABI from "../abi/KIP17TokenABI.json";
 import {
   ACCESS_KEY_ID,
   SECRET_ACCESS_KEY,
   COUNT_CONTRACT_ADDRESS,
+  MARKET_CONTRACT_ADDRESS,
+  NFT_CONTRACT_ADDRESS,
   CHAIN_ID,
 } from "../constants";
+
 const option = {
   headers: [
     {
@@ -25,11 +29,34 @@ const caver = new Caver(
   )
 );
 
-const CountContract = new caver.contract(CounterABI, COUNT_CONTRACT_ADDRESS);
+const NFTContract = new caver.contract(KIP17ABI, NFT_CONTRACT_ADDRESS);
 
-export const readCount = async () => {
-  const _count = await CountContract.methods.count().call();
-  console.log(_count);
+export const fetchCardsOf = async (address) => {
+  // fetch balance
+  const balance = await NFTContract.methods.balanceOf(address).call();
+  console.log("Balance", balance);
+
+  // fetch token IDs
+  const tokenIds = [];
+  for (let i = 0; i < balance; i++) {
+    const id = await NFTContract.methods.tokenOfOwnerByIndex(address, i).call();
+    tokenIds.push(id);
+  }
+
+  // fetch token URIs
+  const tokenUris = [];
+  for (let i = 0; i < balance; i++) {
+    const id = await NFTContract.methods.tokenURI(tokenIds[i]).call();
+    tokenUris.push(id);
+  }
+
+  const nfts = [];
+  for (let i = 0; i < balance; i++) {
+    nfts.push({ uri: tokenUris[i], id: tokenIds[i] });
+  }
+  console.log("nft", nfts);
+
+  return nfts;
 };
 
 export const getBalance = (address) => {
@@ -40,6 +67,14 @@ export const getBalance = (address) => {
     console.log("Balance", balance);
     return balance;
   });
+};
+/*
+
+const CountContract = new caver.contract(CounterABI, COUNT_CONTRACT_ADDRESS);
+
+export const readCount = async () => {
+  const _count = await CountContract.methods.count().call();
+  console.log(_count);
 };
 
 export const setCount = async (newCount) => {
@@ -59,3 +94,4 @@ export const setCount = async (newCount) => {
     console.log("Error set count", e);
   }
 };
+*/
